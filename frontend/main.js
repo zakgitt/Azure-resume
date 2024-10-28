fa66ee303d5af11ca09ffc492f66e2a935188dc9
Zakariya <pro101@live.co.uk>
2024-10-28 11:39:07 +0000

    Updates to frontend strutcure, created main.js file

--- frontend/main.js
+++ frontend/main.js
@@ -7,10 +7,10 @@ const funtionapi = '';
  const getVisitCount = () => {
     let count = 30 
     fetch (funtionapi). then (Response => {
-        return Response.json ()
-    }).then(Response =>{
+        return response.json ()
+    }).then(response =>{
         console.log("website called function API");
-        count = response.count;
+        count =  response.count;
         document.getElementById("counter").innerText = count;
     }).catch(function(error){
         console.log(error);
@@ -18,4 +18,3 @@ const funtionapi = '';
  return count; 
     }
     
- 
